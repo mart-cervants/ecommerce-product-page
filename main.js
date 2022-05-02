2ebb05e9e64f42030ef9857580b4e4b91a4f4504
MACB <mart.cervants@gmail.com>
2022-05-02 17:48:27 -0500

    Adding Offcanvas menu & Cart-info

---
+++ main.js
@@ -0,0 +1,9 @@
+// Showing cart
+const cartIcon = $(".cart-icon img");
+
+//   const cart = $("#cart");
+
+cartIcon.on("click", () => {
+  console.log("click");
+  //   cart.toggleClass("show-cart");
+});
