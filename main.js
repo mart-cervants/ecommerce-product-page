9a053d5df80996fbd7c1ca1d224aee1d68d02a6a
MACB <mart.cervants@gmail.com>
2022-05-06 18:02:11 -0500

    full cart info and counter of items added

--- main.js
+++ main.js
@@ -1,9 +1,40 @@
-// Showing cart
+//===== showing cart-info =====
 const cartIcon = $(".cart-icon img");
-
-//   const cart = $("#cart");
+const cart = $("#cart");
 
 cartIcon.on("click", () => {
-  console.log("click");
-  //   cart.toggleClass("show-cart");
+  cart.toggleClass("show-cart");
 });
+//===== end of showing cart-info =====
+
+//===== add items to cart =====
+
+//===== end of add items to cart =====
+
+//===== add items to counter =====
+const plusIcon = $(".plus-icon h2");
+const minusIcon = $(".minus-icon h2");
+const totalCount = $(".total-selection h2");
+let numCounter = 1;
+
+plusIcon.on("click", () => {
+  numCounter++;
+  totalCount[0].childNodes[0].data = numCounter;
+});
+
+minusIcon.on("click", () => {
+  if (numCounter > 1) {
+    numCounter--;
+    totalCount[0].childNodes[0].data = numCounter;
+  }
+});
+//===== end of add items to counter =====
+
+//===== add to car =====
+const addToCarBtn = $(".add-button button");
+
+addToCarBtn.on("click", () => {
+  // first shows empty cart box
+  // when click add to cart box element
+});
+//===== end of add to car =====
